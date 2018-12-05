4120068deec7d64e60869d133ea2c97814f59acb
Alejandra Gomez <alejita.9813.dg@gmail.com>
2018-12-05 12:58:29 -0500

    Lab completo con .astah

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -6,13 +6,13 @@ var app = (function () {
             this.y=y;
         }        
     }
-    
+
     class Polygon {
 	constructor(points) {
 	    this.points = points;
 	}
     }
-     const POLYGON_FILL_COLOR = '#f00';
+    const POLYGON_FILL_COLOR = '#f00';
     
     var stompClient = null;
 
@@ -23,21 +23,21 @@ var app = (function () {
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
+
     var addPolygonToCanvas = function (polygon) {        
         var canvas = document.getElementById("canvas");
 	var c2 = canvas.getContext('2d');
 	c2.fillStyle = POLYGON_FILL_COLOR;
 	
 	let points = polygon.points;
- 	c2.beginPath();
+	c2.beginPath();
 	c2.moveTo(points[0].x, points[0].y);
- 	let i = 1;
+	let i = 1;
         while(i < points.length) {
 	    c2.lineTo(points[i].x, points[i].y);
 	    ++i;
 	}
- 	c2.closePath();
+	c2.closePath();
 	c2.fill();
     };
     
@@ -53,7 +53,7 @@ var app = (function () {
     const TOPIC_POINT_ADDRESS = '/topic/newpoint';
     const TOPIC_POLYGON_ADDRESS = '/topic/newpolygon';
     const PUBLISH_ADDRESS = '/app/newpoint';
-    
+	
     let pointConnectionAddress = null;
     let polygonConnectionAddress = null;
     let publishAddress = null;
@@ -67,17 +67,18 @@ var app = (function () {
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
 
-            pointConnectionAddress = TOPIC_POINT_ADDRESS + '.' + sessionId;
+	    pointConnectionAddress = TOPIC_POINT_ADDRESS + '.' + sessionId;
 	    polygonConnectionAddress = TOPIC_POLYGON_ADDRESS + '.' + sessionId;
-            publishAddress = PUBLISH_ADDRESS + '.' + sessionId;
+	    publishAddress = PUBLISH_ADDRESS + '.' + sessionId;
 
             stompClient.subscribe(pointConnectionAddress, function (eventbody) {
-                var point = JSON.parse(eventbody.body);
+                // console.log('Eventbody', eventbody);
+		var point = JSON.parse(eventbody.body);
 		console.log('Parsed JSON point:', point);
-                addPointToCanvas(point);
+		addPointToCanvas(point);
             });
 
-            stompClient.subscribe(polygonConnectionAddress, function (eventbody) {
+	    stompClient.subscribe(polygonConnectionAddress, function (eventbody) {
                 // console.log('Eventbody', eventbody);
 		var polygon = JSON.parse(eventbody.body);
 		console.log('Parsed JSON polygon:', polygon);
@@ -86,15 +87,15 @@ var app = (function () {
         });
 
     };
-    
+
     var sessionId = null;
     
     var init = function () {
         var can = document.getElementById("canvas");
 
-        $('#sendPointBtn').prop('disabled', true);
-        
-        $('#connectBtn').click(function () {
+	$('#sendPointBtn').prop('disabled', true);
+	
+	$('#connectBtn').click(function () {
 	    sessionId = Number($('#sessionIdentifier').val());
 	    if (isNaN(sessionId)) {
 		sessionId = null;
@@ -102,30 +103,41 @@ var app = (function () {
 		// websocket connection
 		connectAndSubscribe(sessionId);
 		$('#sendPointBtn').prop('disabled', false);
- 		$('#connectBtn').prop('disabled', true);
+		$('#connectBtn').prop('disabled', true);
 	    }
 	});
-        $('#sendPointBtn').click(function () {
+	
+	$('#sendPointBtn').click(function () {
 	    var x = $('#x').val();
 	    var y = $('#y').val();
 	    if (sessionId != null) {
 		publishPoint(x, y);
 	    }
 	});
-    };	
+
+	$('#canvas').click(function (event) {
+	    let canvasPoint = getMousePosition(event);
+	    if (sessionId != null) {
+		publishPoint(canvasPoint.x, canvasPoint.y);
+	    }
+	});
+    };
+
     var publishPoint = function(px, py){
-        if (sessionId == null) {
+	if (sessionId == null) {
 	    alert('No se ha establecido el numero de sesion');
 	    return;
 	}
 	
-        var pt = new Point(px,py);
-        console.info("publishing point at "+pt);
+        var pt = new Point(px, py);
+        console.info("publishing point at " + pt);
         addPointToCanvas(pt);
 
         // publish the event
-	stompClient.send(connectionAddress, {}, JSON.stringify(pt));
+	console.log(publishAddress);
+	stompClient.send(publishAddress, {}, JSON.stringify(pt));
     };
+
     var disconnect = function () {
         if (stompClient !== null) {
             stompClient.disconnect();
@@ -133,12 +145,13 @@ var app = (function () {
         setConnected(false);
         console.log("Disconnected");
     };
-    
+
     /* =============== */
+    
     return {
         init: init,
         publishPoint: publishPoint,
         disconnect: disconnect
     };
- 	 
- })();
+
+})();
